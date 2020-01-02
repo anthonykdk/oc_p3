34e3d40fb75631af99b7a9506ac82ec0afb21f61
anthonykdk <anthony@kondek.fr>
2020-01-02 18:52:36 +0100

    Ajout lien Mon compte et Deconnexion dans le header et nom et prenom utilisateur en dynamique mais pas encore ok

--- header.php
+++ header.php
@@ -1,9 +1,38 @@
 <header>
-		<figure>
-			<a href="index.php"><img id="logo_header" src="images/logo.png" alt="logo de GBAF" /></a>
-		</figure>	
-		<div id="nom_prenom_utilisateur">
-				<img id="logo_login" src="images/logo_login.png" alt="icone utilisateur">
-				Nom et prénom
+
+<?php
+	if (isset($_POST['username_login']) AND $_POST['username_login'] !=  NULL AND isset($_POST['password_login']) AND $_POST['password_login'] !=  NULL )
+{
+?>	
+	<div id="myaccount"><a href="#">Mon compte</a> | <a href="#">Deconnexion</a></div>
+<?php
+}
+?>
+
+	<div id="header_content">
+
+		<div class="elementh_logo">
+			<figure>
+				<a href="index.php"><img id="logo_header" src="images/logo.png" alt="logo de GBAF" /></a>
+			</figure>	
 		</div>
+
+		<div class="namefirstame"> 
+			<?php if (isset($_POST['username_login']) AND $_POST['username_login'] !=  NULL AND isset($_POST['password_login']) AND $_POST['password_login'] !=  NULL )
+				{
+			?>
+				<img id="logo_login" src="images/logo_login.png" alt="icone utilisateur">
+
+			<?php
+				 echo $_POST['username_login'];?> <?php echo $_POST['password_login'];
+				}
+			?>
+
+		</div>
+	
+	</div>
+
 </header>
+
+
+
